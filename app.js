87ef5ebdce594df41db57459d2270f3f3bd45421
efradxxd <efradxxd@gmail.com>
2018-12-20 11:11:44 -0600

    primer push

---
+++ app.js
@@ -0,0 +1,43 @@
+var mysql = require('mysql');
+var express = require('express');
+var cors = require('cors');
+
+var app = express();
+
+app.use(cors({
+	'allowedHeaders': ['sessionId', 'Content-Type'],
+	'exposedHeaders': ['sessionId'],
+	'origin': '*',
+	'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	'preflightContinue': false
+}));
+
+app.use(function(req, res, next){
+	res.locals.connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'root',
+		password : 'root',
+		database : 'sat'
+	});
+	res.locals.connection.connect();
+	next();
+});
+
+
+app.get('/api/:cer/:key/:pass', function(req, res, next) {
+	res.locals.connection.query("SELECT * FROM users WHERE certificado='" + req.params.cer + "' AND keysat='" + req.params.key + "' AND pass='" + req.params.pass + "'", function (error, results, fields) {
+       
+        if(results.length > 0){
+            res.jsonp({status: 200, error: null, userStatus: "Accept"});
+        }else{
+            res.jsonp({status: 200, error: null, userStatus: "Reject"});
+		}
+		if (error) throw error;
+		
+	});
+});
+
+app.listen(3000, () =>{
+    console.log("Escuchando por el puerto 3000");
+})
+  
